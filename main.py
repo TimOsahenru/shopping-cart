13552068b1d6b0190cc995741007a18cfc3e623d
timosahenru <timosahenru@gmail.com>
2023-06-03 09:42:26 +0000

    add order items created

---
+++ main.py
@@ -0,0 +1,22 @@
+class Customer():
+    def __init__(self, username):
+        self.username = username
+        
+class Product():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+class Order():
+    def __init__(self, customer):
+        self.customer = customer
+        self.order_items = []
+        
+        
+    def add_items(self, item):
+        self.order_items.append(item)
+    
+    
+class OrderItem():
+    def __init__(self, product, quantity):
+        pass
---
+++ tests.py
@@ -0,0 +1,25 @@
+import unittest
+from main import Order, Customer, Product, OrderItem
+
+
+class TestOrder(unittest.TestCase):
+    def test_add_order_items(self):
+        customer = Customer("Tim")
+        order = Order(customer)
+        
+        product_1 = Product("Oranges", 20)
+        product_2 = Product("Mangoes", 30)
+        product_3 = Product("Salad", 40)
+        
+        order_item_1 = OrderItem(product_1, 3)
+        order_item_2 = OrderItem(product_2, 6)
+        
+        order.add_items(order_item_1)
+        order.add_items(order_item_2)
+        
+        self.assertEqual(len(order.order_items), 2)
+        self.assertIn(order_item_1, order.order_items)
+        self.assertEqual(order.order_items[0], order_item_1)
+        
+if __name__ == "__main__":
+    unittest.main()
